ce677eed8dbd9908f06278f0a4db1c25d125da06
Kevin kamla <kvnkamla4@gmail.com>
2022-12-02 00:55:35 +0100

    tracemp3

--- src/app/views/play/play.page.ts
+++ src/app/views/play/play.page.ts
@@ -11,9 +11,11 @@ import { HandleMusicService } from 'src/app/service/handle-music.service';
 })
 export class PlayPage implements OnInit {
 
+  public isPlay: boolean = false
   tabAlbum: any = [];
   tabSon: any = [];
   currentSon: any;
+  sonPrecedent: any;
   currentAlbum: any;
   duration: any;
   min: any;
@@ -23,15 +25,38 @@ export class PlayPage implements OnInit {
   currentValueDuration: number = 0;
   pauseIcon: string = "pause";
   currentTabSon: any;
-  volSon: number = 0.4
+  volSon: number = 0.5
   muteIcon: string = "mute"
-  temps: any = 1000
+  sonIsEnd: boolean = false
+  gesVol: string = "medium"
 
   constructor(private modal: ModalController, private service: HandleMusicService) { }
 
   // Fermeture du modal(page play)
   closeModal() {
     this.modal.dismiss();
+
+  }
+  // Modification de l'instance son son 
+  rangeChange(e: any) {
+    const t = e.target.value
+    this.currentValueDuration = t
+
+    if (this.secInit < 59) {
+      this.secInit += 1
+    } else {
+      this.minInit += 1
+      this.secInit = 0
+      // this.minInit = Math.floor(t / 60)
+
+    }
+
+    if (t == Math.floor(this.duration)) {
+      this.pauseIcon = "play"
+      // this.currentValueDuration = 0
+      this.sonIsEnd = true
+      // this.service.playSon(this.currentSon, t)
+    }
   }
 
   // Son precedent
@@ -56,11 +81,15 @@ export class PlayPage implements OnInit {
 
   pauseSon() {
     if (this.pauseIcon == "pause") {
+      this.sonIsEnd = false
       this.pauseIcon = "play"
       this.service.pause(this.currentSon)
     } else {
       this.pauseIcon = "pause"
-      this.service.resume(this.currentSon)
+      if (this.sonIsEnd) {
+        this.currentValueDuration = 0;
+        this.service.playSon(this.currentSon, 0.0)
+      } else this.service.resume(this.currentSon)
     }
   }
 
@@ -87,23 +116,51 @@ export class PlayPage implements OnInit {
     this.service.stopSon(this.currentSon)
     this.currentValueDuration = Math.floor(this.duration)
     this.pauseIcon = "play"
+    this.minInit = 0
+    this.secInit = 0
+    this.sonIsEnd = true
   }
 
-  // Modification de l'instance son son 
-  rangeChange(e: any) {
-    const t = e.target.value
-    // console.log("time change : ", t);
-    this.currentValueDuration = t
-    if (this.minInit < this.min) {
-      this.minInit = Math.floor(t / 60)
-      this.secInit = Math.floor(((t / 60) - this.minInit / 60) * 60)
+  //   ================== Gestion du volume ======================
+
+  plus() {
+    if (this.gesVol == "mute") {
+      this.gesVol = "low"
     } else {
-      if (this.minInit = this.min) {
-        if (this.secInit < this.sec) this.secInit = Math.floor(((t / 60) - this.minInit / 60) * 60)
+      if (this.gesVol == "low") {
+        this.gesVol = "medium"
+        this.service.setVolume(this.currentSon, this.volSon + 0.2);
+        this.volSon += 0.2;
+      } else
+        if (this.gesVol == "medium") {
+          this.gesVol = "high"
+          this.service.setVolume(this.currentSon, this.volSon + 0.2);
+          this.volSon += 0.2;
+        } else {
+          if (this.gesVol == "high") {
+            this.service.setVolume(this.currentSon, this.volSon + 0.2);
+            this.volSon += 0.2;
+          }
+        }
+    }
+  }
+
+  manus() {
+    if (this.gesVol == "high") {
+      this.gesVol = "medium"
+      this.service.setVolume(this.currentSon, this.volSon - 0.2);
+      this.volSon -= 0.2;
+    } else {
+      if (this.gesVol == "medium") {
+        this.gesVol = "low"
+        this.service.setVolume(this.currentSon, this.volSon - 0.2);
+        this.volSon -= 0.2;
+      } else {
+        this.gesVol = "mute"
+        this.service.setVolume(this.currentSon, this.volSon - 0.2);
+        this.volSon -= 0.2;
       }
     }
-
-    // this.service.playSon(this.currentSon, t)
   }
 
   // Baisser le volume
@@ -136,6 +193,8 @@ export class PlayPage implements OnInit {
   // =========================================== OnInit ==========================================
   ngOnInit() {
     var sonIdPreview = localStorage.getItem('sonIdPreview');
+    this.sonPrecedent = this.tabSon.filter((x: { id: any }) => x.id == sonIdPreview)[0];
+    this.isPlay ? this.service.stopSon(this.sonPrecedent) : "";
     var sonId = (localStorage.getItem('sonIdSelected'));
     localStorage.removeItem("sonIdSelected")
     localStorage.setItem("sonIdPreview", JSON.stringify(sonId));
@@ -151,6 +210,7 @@ export class PlayPage implements OnInit {
         this.currentSon = this.tabSon.filter((x: { id: any }) => x.id == sonId)[0];
         console.log(" son courant : ", this.currentSon)
         this.service.playSon(this.currentSon, 0.0)
+        this.isPlay = true
       }
 
       // ===================== Duree du son ====================================  
@@ -158,20 +218,20 @@ export class PlayPage implements OnInit {
         this.service.getDuration(this.currentSon).then((e) => {
           this.duration = e.duration;
           this.min = Math.floor(this.duration / 60)
-          this.sec = Math.floor(((this.duration / 60) - this.min / 60) * 60)
+          this.sec = Math.floor(this.duration - this.min * 60)
           console.log("min : ", this.min, "sec : ", this.sec)
         })
+      }, 1000)
 
-        setInterval(() => {
-          console.log("duree son : ", this.duration)
-          if (this.currentValueDuration == Math.floor(this.duration)) {
-            this.temps = null
-          } else {
-            this.currentValueDuration += 1
-            console.log("progress bar ..... : ", this.currentValueDuration);
-          }
-        }, this.temps)
-      }, this.temps)
+      setInterval(() => {
+        console.log("range courrant : ", this.currentValueDuration, "\n duree : ", Math.floor(this.duration))
+        if (this.currentValueDuration == Math.floor(this.duration)) {
+
+        } else {
+          this.currentValueDuration += 1
+          console.log("progress bar ..... : ", this.currentValueDuration);
+        }
+      }, 1000)
     });
     // ===================== Les albums =====================================================
 
